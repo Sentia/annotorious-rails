8e788474fa0b0e756e1cc1666a72dc15c965668d
Liam Keene <liam.keene@gmail.com>
2017-04-07 18:02:44 +1000

    Initial commit

---
+++ lib/annotorious/rails.rb
@@ -0,0 +1,8 @@
+require "annotorious/rails/version"
+
+module Annotorious
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/annotorious/rails/version.rb
@@ -0,0 +1,5 @@
+module Annotorious
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
